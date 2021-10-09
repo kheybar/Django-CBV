f6a166fa655f6f8eafebe1b26e843e41b92c58e2
kheybar <mahdizarepour15@gmail.com>
2021-10-09 19:54:31 +0330

    this branch(View) about CBV View. (E2)

--- first/views.py
+++ first/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.views import View
 
 
+class Home(View):
+    
+    def get(self, request, *args, **kwargs):
+        context = {
+            'name': 'BOSS',
+        }
+        return render(request=request, template_name='first/home.html', context=context)
 
-def home(request):
-    return render(request=request, template_name='first/home.html')
