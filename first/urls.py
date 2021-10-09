776e72cfdb4cc3f46303d261f9014f589e5e1647
kheybar <mahdizarepour15@gmail.com>
2021-10-09 20:26:40 +0330

    this is TemplateView branch and about Base TemplateView. (E3)

--- first/urls.py
+++ first/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+from django.views.generic.base import TemplateView # with out write class
+
 app_name = 'first'
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.Home.as_view(), name='home'),
+    path('', TemplateView.as_view(template_name='first/home.html'), name='home'), # good for static page
 ]
--- first/views.py
+++ first/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from .models import Todo
 
 
+class Home(TemplateView):
+    template_name = 'first/home.html'
 
-def home(request):
-    return render(request=request, template_name='first/home.html')
+    # todos = Todo.objects.all() # no class Atribute
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['todos'] = Todo.objects.all() # Correct
+        return context
