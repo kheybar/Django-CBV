33ad997632bfa952dcd21579678079db9b07033f
kheybar <mahdizarepour15@gmail.com>
2021-10-09 21:00:55 +0330

    this is branch(ListView) and about CBV ListView. (E4)

--- first/views.py
+++ first/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+
+from .models import Todo
 
 
+class Home(ListView):
+    template_name = 'first/home.html' # default 'app_name/modelname_list.html'
+    context_object_name = 'todos' # default object_list
 
-def home(request):
-    return render(request=request, template_name='first/home.html')
+    # queryset = Todo.objects.filter() # good for normal query
+    # model = Todo # for get all objects from model
+    def get_queryset(self):
+        return Todo.objects.all().order_by('-created')
+    
+    # ordering = ['-created']
+    # paginated_by = 9
+
+
