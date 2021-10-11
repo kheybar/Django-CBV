15c1910ad2a8974bf6869dea3beba15bbfe926c3
kheybar <mahdizarepour15@gmail.com>
2021-10-11 23:27:42 +0330

    this branch(DetailView) about CBV DetailView.

--- first/urls.py
+++ first/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'first'
 urlpatterns = [
-    path('', views.Home.as_view(), name='home')
+    path('', views.Home.as_view(), name='home'),
+    path('<int:pk>/<slug:article_slug>/', views.DetailTodo.as_view(), name='detail_todo')
 ]
--- first/views.py
+++ first/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
-from django.views import View
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Todo
 
 
-class Home(View):
-    
-    def get(self, request, *args, **kwargs):
-        context = {
-            'name': 'BOSS',
-        }
-        return render(request=request, template_name='first/home.html', context=context)
+class Home(ListView):
+    template_name = 'first/home.html'
+    model = Todo
+    context_object_name = 'todos'
+    ordering = ['-created']
+
+
+
+class DetailTodo(DetailView):
+    template_name = 'first/detail.html'
+    model = Todo
+    context_object_name = 'todo'
+    slug_field = 'slug'
+    slug_url_kwarg = 'article_slug'
+
+    def get_queryset(self, **kwargs):
+        return Todo.objects.filter(id=self.kwargs['pk'], slug__iexact=self.kwargs['article_slug'])
+
 
