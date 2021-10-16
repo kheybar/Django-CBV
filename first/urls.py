cee4c7f5282eae5dae7bdc7325787ff6bcbfaa9c
mahdi zarepour <mahdizarepour15@gmail.com>
2021-10-16 15:55:02 +0330

    change master and update to UpdateView.

--- first/urls.py
+++ first/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'first'
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
-    path('<int:pk>/<slug:article_slug>/', views.DetailTodo.as_view(), name='detail_todo')
+    path('create/', views.CreateTodo.as_view(), name='create_todo'),
+    path('delete/<int:pk>/', views.DeleteTodo.as_view(), name='delete_todo'),
+    path('update/<int:pk>/', views.UpdateTodo.as_view(), name='update_todo'),
+    path('<int:pk>/<slug:article_slug>/', views.DetailTodo.as_view(), name='detail_todo'),
 ]
--- first/views.py
+++ first/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
+from django.utils.text import slugify
+from django.contrib import messages
 from .models import Todo
 
 
@@ -22,4 +23,39 @@ class DetailTodo(DetailView):
     def get_queryset(self, **kwargs):
         return Todo.objects.filter(id=self.kwargs['pk'], slug__iexact=self.kwargs['article_slug'])
 
+
+
+class CreateTodo(CreateView):
+    model = Todo
+    fields = ('title',)
+    template_name = 'first/create.html'
+    success_url = reverse_lazy('first:home')
+
+    def form_valid(self, form):
+        todo = form.save(commit=False)
+        todo.slug = slugify(form.cleaned_data['title'])
+        todo.save()
+        messages.success(self.request, 'your todo add successfully.', extra_tags='success')
+        return super().form_valid(form)
+
+
+
+class DeleteTodo(DeleteView):
+    model = Todo
+    template_name = 'first/delete.html'
+    success_url = reverse_lazy('first:home')
+
+
+class UpdateTodo(UpdateView):
+    model = Todo
+    fields = ('title',)
+    template_name = 'first/update.html'
+    success_url = reverse_lazy('first:home')
+
+    def form_valid(self, form):
+        todo = form.save(commit=False)
+        todo.slug = slugify(form.cleaned_data['title'])
+        todo.save()
+        messages.success(self.request, 'your todo update successfully.', extra_tags='success')
+        return super().form_valid(form)
 
