33f8dafe545cb6384e551793aac6282f8a127fb8
mahdi zarepour <mahdizarepour15@gmail.com>
2021-10-25 17:06:12 +0330

    FormMixin add to Project.

--- first/urls.py
+++ first/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('create/', views.CreateTodo.as_view(), name='create_todo'),
     path('delete/<int:pk>/', views.DeleteTodo.as_view(), name='delete_todo'),
     path('update/<int:pk>/', views.UpdateTodo.as_view(), name='update_todo'),
-    path('<int:pk>/<slug:article_slug>/', views.DetailTodo.as_view(), name='detail_todo'),
+    path('<int:pk>/<slug:todo_slug>/', views.DetailTodo.as_view(), name='detail_todo'),
     path('archive/<int:year>/<int:month>/', views.MonthTodo.as_view(), name='month_todo'),
 ]
--- first/views.py
+++ first/views.py
@@ -1,10 +1,13 @@
 from django.urls import reverse_lazy
+from django.urls import reverse
 from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
+from django.views.generic.edit import FormMixin
 from django.views.generic.dates import MonthArchiveView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils.text import slugify
 from django.contrib import messages
-from .models import Todo
+from .models import Todo, Comment
+from .forms import CommentForm
 
 
 class Home(ListView):
@@ -15,7 +18,7 @@ class Home(ListView):
 
 
 
-class DetailTodo(LoginRequiredMixin, DetailView):
+class DetailTodo(LoginRequiredMixin, FormMixin, DetailView):
     template_name = 'first/detail.html'
     # model = Todo
     context_object_name = 'todo'
@@ -23,7 +26,24 @@ class DetailTodo(LoginRequiredMixin, DetailView):
     # slug_url_kwarg = 'article_slug'
 
     def get_queryset(self, **kwargs):
-        return Todo.objects.filter(id=self.kwargs['pk'], slug__iexact=self.kwargs['article_slug'])
+        return Todo.objects.filter(id=self.kwargs['pk'], slug__iexact=self.kwargs['todo_slug'])
+    
+
+    # FormMixin
+    form_class = CommentForm
+
+    def get_success_url(self):
+        return reverse('first:detail_todo', kwargs={'pk': self.object.pk, 'todo_slug': self.object.slug})
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object() # اون آبجکتی که کاربر داره براش فرم پر میکنه
+        form = self.get_form() # برای دریافت فرم پر شده توسط کاربر از این متود استفاده میکنیم
+        if form.is_valid():
+            cd = form.cleaned_data
+            create_comment = Comment(todo=self.object, name=cd['name'], body=cd['body'])
+            create_comment.save()
+            messages.success(request=request, message='your todo comment successfully', extra_tags='success')
+        return super().form_valid(form)
 
 
 
@@ -37,7 +57,7 @@ class CreateTodo(LoginRequiredMixin, CreateView):
         todo = form.save(commit=False)
         todo.slug = slugify(form.cleaned_data['title'])
         todo.save()
-        messages.success(self.request, 'your todo add successfully.', extra_tags='success')
+        messages.success(self.request, 'your todo add successfully', extra_tags='success')
         return super().form_valid(form)
 
 
@@ -46,6 +66,7 @@ class DeleteTodo(LoginRequiredMixin, DeleteView):
     model = Todo
     template_name = 'first/delete.html'
     success_url = reverse_lazy('first:home')
+
 
 
 class UpdateTodo(LoginRequiredMixin, UpdateView):
